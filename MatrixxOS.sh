ab35e7e4b0ebb034ec0d4a9a03b80c1bc5a66664
dpenra <dpenra.stha@gmail.com>
2024-10-06 22:46:05 +0545

    update matrixos

--- MatrixxOS.sh
+++ MatrixxOS.sh
@@ -64,7 +64,7 @@ git clone https://github.com/KProfiles/android_packages_apps_Kprofiles.git --dep
 git clone https://github.com/xiaomi-begonia-dev/android_packages_apps_ViPER4AndroidFX.git --depth 1 -b fourteen packages/apps/ViPER4AndroidFX
 # source mods
 git clone https://github.com/yaap/hardware_qcom-caf_sm8150_media.git --depth 1 -b fourteen hardware/qcom-caf/sm8150/media
-git clone https://github.com/dpenra-sunny/android_vendor_lineage.git --depth 1 -b fourteen vendor/lineage
+git clone https://github.com/matrixx-sunny/android_vendor_lineage.git --depth 1 -b fourteen vendor/lineage
 # set build environment
 . build/envsetup.sh
 # lunch
