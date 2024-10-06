ae3420df8da2abfd20214f32f46678a27fc75d35
Deependra Shrestha <36838592+dpenra@users.noreply.github.com>
2024-10-06 22:11:40 +0545

    Update riseos.sh

--- riseos.sh
+++ riseos.sh
@@ -31,17 +31,16 @@ rm -rf hardware/qcom-caf/sm8150/media
 rm -rf packages/apps/Updater
 rm -rf vendor/lineage
 # Initialize ROM manifest
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
+repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
 # repo sync
 /opt/crave/resync.sh
 # remove directory
 rm -rf hardware/qcom-caf/sm8150/media
 rm -rf vendor/lineage
-rm -rf packages/apps/Updater
 rm -rf frameworks/base
 # cloning DT
 # device tree
-git clone https://github.com/dpenra-sunny/device_xiaomi_sunny.git --depth 1 -b fourteen device/xiaomi/sunny
+git clone https://github.com/matrixx-sunny/device_xiaomi_sunny.git --depth 1 -b fourteen device/xiaomi/sunny
 git clone https://github.com/dpenra-sunny/device_xiaomi_sunny-kernel.git --depth 1 -b fourteen device/xiaomi/sunny-kernel
 git clone https://github.com/AOSPA/android_device_qcom_qssi.git --depth 1 -b uvite device/qcom/qssi
 git clone https://github.com/yaap/device_qcom_common.git --depth 1 -b fourteen device/qcom/common
@@ -66,12 +65,11 @@ git clone https://github.com/KProfiles/android_packages_apps_Kprofiles.git --dep
 git clone https://github.com/xiaomi-begonia-dev/android_packages_apps_ViPER4AndroidFX.git --depth 1 -b fourteen packages/apps/ViPER4AndroidFX
 # source mods
 git clone https://github.com/yaap/hardware_qcom-caf_sm8150_media.git --depth 1 -b fourteen hardware/qcom-caf/sm8150/media
-git clone https://github.com/dpenra-sunny/android_packages_apps_Updater.git --depth 1 -b fourteen packages/apps/Updater
 git clone https://github.com/dpenra-sunny/android_vendor_lineage.git --depth 1 -b fourteen vendor/lineage
 git clone https://github.com/dpenra-sunny/android_frameworks_base.git --depth 1 -b fourteen frameworks/base
 # set build environment
 . build/envsetup.sh
 # lunch
-riseup sunny user
+brunch sunny user
 # make
-rise b
+# rise b
