1a1866714c87b02ebac600df59d52cbb254531d6
Deependra Shrestha <36838592+dpenra@users.noreply.github.com>
2024-10-08 21:51:39 +0545

    Update MatrixxOS.sh

--- MatrixxOS.sh
+++ MatrixxOS.sh
@@ -66,7 +66,6 @@ git clone https://github.com/matrixx-sunny/android_vendor_lineage.git --depth 1 
 git clone https://github.com/matrixx-sunny/android_vendor_lineage-priv_keys-template.git --depth 1 -b master vendor/lineage-priv/keys
 # set build environment
 . build/envsetup.sh
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 cd vendor/lineage-priv/keys
 ./generate.sh
 cd ..
